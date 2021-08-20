f758cf355214be1e5704c5483b788766750d38b6
Vegeta Saiyan <sadmansamin225@gmail.com>
2021-08-19 21:49:09 -0400

    completed mvp tasks

--- index.js
+++ index.js
@@ -199,9 +199,20 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   
+class ProjectManager extends Instructor {
+  constructor (atrs) {
+    super(atrs);
+    this.gradClassName = atrs.gradClassName;
+    this.favInstructor = atrs.favInstructor;
+  }
+  standUp(slackChannel) {
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`
+  }
+  debugsCode(student, subject) {
+    return `${this.name} debugs ${student.name}'s code on ${subject}.`
+  }
 }
+
 /*
   STRETCH PROBLEM (no tests!)
     - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
